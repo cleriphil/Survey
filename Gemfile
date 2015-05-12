09fbea513216b83c80fc14405db6c2a1d512c0c9
Kim Dang & cleri philiastides <student@epicodus.com>
2015-05-12 09:32:49 -0700

    Add initial files

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source('https://rubygems.org')
+
+gem('sinatra')
+gem('pg')
+gem('rspec')
+gem('sinatra-contrib', :require => 'sinatra/reloader')
+gem('sinatra-activerecord')
+gem('rake')
---
+++ db/migrate/20150512163103_create_questions_table.rb
@@ -0,0 +1,10 @@
+class CreateQuestionsTable < ActiveRecord::Migration
+  def change
+    create_table(:questions) do |t|
+      t.column(:description, :string)
+      t.column(:survey_id, :integer)
+
+      t.timestamps()
+    end
+  end
+end
